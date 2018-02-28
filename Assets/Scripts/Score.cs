5e0a9ebcd052477f80598313bae8ba6e3ee22f72
dev001hajipro <dev001hajipro@gmail.com>
2018-03-01 06:27:10 +0900

    スコア実装、テスト追加

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.UI;
+using System.Globalization;
+using DG.Tweening;
 
-public class Score : MonoBehaviour {
+[RequireComponent (typeof(Text))]
+public class Score : MonoBehaviour
+{
+	public int addValue = 10;
+	public Text scoreText;
 
-	// Use this for initialization
-	void Start () {
-		
+	// TODO: DOTween非同期のテスト追加。テストの書きかたが複雑そうなので後回し。
+	public void UpWithAnime ()
+	{
+		int endValue = Value + addValue;
+		DOTween.To (() => Value, (n) => Value = n, endValue, 0.5f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public int Value {
+		get {
+			// 00001のスタイルを1に変換
+			return int.Parse (scoreText.text, NumberStyles.Any);
+		}
+		set {
+			scoreText.text = value.ToString ("00000");
+		}
 	}
 }
---
+++ Assets/Scripts/ScorePlayModeTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Analytics;
+
+public class ScorePlayModeTest
+{
+
+	[Test]
+	public void ScorePlayModeTestValue ()
+	{
+		// 初期化
+		var obj = new GameObject ("ScoreObject", new [] {
+			typeof(Score)
+		});
+		var script = obj.GetComponent<Score> ();
+		Assert.NotNull (script);
+		script.scoreText = obj.GetComponent<Text> (); // 何故Textを持つ?
+		Debug.Log (obj.GetComponent<Text> ());
+		script.scoreText.text = 0.ToString ("00000");
+		Assert.AreEqual (0, script.Value);
+
+		// 点数を足したときのテスト
+		int expectedSum = script.Value + script.addValue;
+		script.Value += script.addValue;
+		Assert.AreEqual (expectedSum, script.Value);
+
+	}
+
+	/*
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator ScorePlayModeTestWithEnumeratorPasses ()
+	{
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+	*/
+}
