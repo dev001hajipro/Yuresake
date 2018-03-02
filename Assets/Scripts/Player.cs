65b0f34bb5234152ef2293b16178ed333f09e41a
dev001hajipro <dev001hajipro@gmail.com>
2018-03-03 07:18:31 +0900

    モバイル入力対応

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements.StyleEnums;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Player : MonoBehaviour
 	private Rigidbody2D rb2d;
 	private Vector2 movement = Vector2.zero;
 	public float speed = 5f;
+	private Vector3 newPos = Vector3.zero;
 
 	void Start ()
 	{
@@ -17,16 +19,36 @@ public class Player : MonoBehaviour
 
 	void Update ()
 	{
+		#if (UNITY_ANDROID || UNITY_IOS)
+		/*
+		transform.position = Vector3.MoveTowards (transform.position, newPos, Time.deltaTime * 100f);
+		if (Input.touchCount > 0) {
+			var touch = Input.touches [0];
+			if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Ended) {
+				newPos = Camera.main.ScreenToWorldPoint (touch.position);
+				newPos.z = transform.position.z;
+			}
+		}
+		*/
+		if (Input.touchCount > 0) {
+			var touch = Input.touches [0];
+			//rb2d.velocity = new Vector2 ((touch.position.x - Screen.width / 2) * 1, rb2d.velocity.y);
+			rb2d.velocity = new Vector2 ((Camera.main.ScreenToWorldPoint (touch.position).x - transform.position.x) * 3, rb2d.velocity.y);
+			Debug.Log ((Camera.main.ScreenToWorldPoint (touch.position).x - transform.position.x) - transform.position.x);
+		}
+		#else
 		float horizontal = Input.GetAxis ("Horizontal");
 		float vertical = Input.GetAxis ("Vertical");
 		movement = new Vector2 (horizontal, vertical);
+		#endif
 	}
 
 	void FixedUpdate ()
 	{
-		//rb2d.AddForce (movement * speed);
-		//rb2d.velocity = movement * speed;
+		#if (UNITY_IOS || UNITY_ANDROID)
+		#else
 		rb2d.AddForce (movement * speed, ForceMode2D.Impulse);
+		#endif
 	}
 
 	void OnTriggerEnter2D (Collider2D other)
