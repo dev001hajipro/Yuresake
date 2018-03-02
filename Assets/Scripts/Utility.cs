4d20b327be2f681c72ef21223f5e385f3e78f351
dev001hajipro <dev001hajipro@gmail.com>
2018-03-02 15:16:40 +0900

    ボタンにアニメとSEつけた

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.EventSystems;
 
 public class Utility : MonoBehaviour
 {
@@ -12,5 +14,27 @@ public class Utility : MonoBehaviour
 	public void LoadScene (int sceneBuildIndex)
 	{
 		SceneManager.LoadScene (sceneBuildIndex);
+	}
+
+	public void GoToMain ()
+	{
+		LoadSceneWithAnime (() => SceneManager.LoadScene (SCENE_MAIN));
+	}
+
+	public void GoToTitle ()
+	{
+		LoadSceneWithAnime (() => SceneManager.LoadScene (SCENE_TITLE));
+	}
+
+	private void LoadSceneWithAnime (TweenCallback callback)
+	{
+		DOTween.Sequence ()
+			.Join (gameObject.GetComponent<RectTransform> ().DOScale (Vector3.one * 1.5f, 0.5f))
+			.Join (gameObject.GetComponent<CanvasGroup> ().DOFade (0.0f, 0.5f))
+			.OnComplete (callback);
+/*		
+		gameObject.GetComponent<RectTransform> ()
+			.DOScale (Vector3.one * 1.5f, 0.3f)
+			.OnComplete (callback);*/
 	}
 }
